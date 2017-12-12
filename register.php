7c4659402ac61bb4509640fef4c0c21dd5598d75
Nerys Jimenez <jimenezpichardon785@strose.edu>
2017-12-11 22:18:55 -0500

    Add files via upload

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+    $con = mysqli_connect("localhost","guest_registration","", "guest_registration") or
+       die("Failed to connect to database " . mysqli_error());
+       	
+	$fname = $_POST["fname"];
+	$lname = $_POST["lname"];
+	$id = $_POST["id"];
+	$comments = $_POST["comments"];
+	$title = $_POST["title"];
+	$sex = $_POST["sex"];
+	
+	$sql = "SELECT * FROM guest_registration WHERE personal_id LIKE '" . $id . "';";
+	
+	if($result = mysql_query($sql) && mysql_num_rows($result) > 0)
+		echo "Guest already exist";
+	else
+	{
+			$SQLstring = "INSERT INTO guest_registration VALUES(" .
+              $id . ",'" . $title . "','" . $fname .
+              "'," . $lname . ",'" . $sex . ",'" . $comments .
+              "');";  
+	
+	echo "Registration successful";
+	}
+
+    mysqli_close($con);
+?>
